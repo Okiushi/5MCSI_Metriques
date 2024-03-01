bdefbeb8c384b9ed469f43eddc9242d8c9e55b16
Okiushi <100536875+Okiushi@users.noreply.github.com>
2024-03-01 12:28:46 +0100

    Commits page fix 21

--- __init__.py
+++ __init__.py
@@ -51,8 +51,6 @@ def commitsdata():
             commits_per_min[minutes] = 1
     for key in commits_per_min:
         results.append({'date': key, 'commits': commits_per_min[key]})
-    print(results)
-    print(json.dumps(results))
     return jsonify(results=results)
 
 @app.route("/commits/")
